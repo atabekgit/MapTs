40fa7a9924876a09a60f59d71bc688ddb0ca2da3
atabe <atabekgithub23>
2022-05-12 18:44:12 +0600

    second

--- src/user.ts
+++ src/user.ts
@@ -7,7 +7,7 @@ export class User{
     lng:number;
   };
   constructor(){
-    this.name = faker.firstName();
+    this.name = faker.name.firstName();
     this.location = {
       lat: parseFloat( faker.address.latitude()),
       lng: parseFloat( faker.address.longitude())
